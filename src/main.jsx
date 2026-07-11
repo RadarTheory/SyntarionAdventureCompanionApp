c5abd049248b8bf48c35a17d2825932f6ab93922
Radar Theory <your-email@example.com>
2026-07-11 17:43:10 -0500

    route /architect-vault-7x2k to AdminPortal

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AdminPortal from './AdminePortal.jsx'
+
+const isAdminRoute = window.location.pathname.startsWith('/architect-vault-7x2k')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isAdminRoute ? <AdminPortal /> : <App />}
   </StrictMode>,
 )
